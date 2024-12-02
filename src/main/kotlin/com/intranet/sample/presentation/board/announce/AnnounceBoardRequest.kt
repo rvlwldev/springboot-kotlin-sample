05f60ffd082d8a7ed95e1b51818b62496c4019a7
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-12-02 22:59:57 +0900

    feat: AnnounceBoard Controller 구현

---
+++ src/main/kotlin/com/intranet/sample/presentation/board/announce/AnnounceBoardRequest.kt
@@ -0,0 +1,46 @@
+package com.intranet.sample.presentation.board.announce
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+import org.hibernate.validator.constraints.Length
+
+class AnnounceBoardRequest {
+
+    data class Post(
+        @field:NotBlank
+        val username: String,
+
+        @field:NotBlank
+        @field:Length(min = 4)
+        val password: String,
+
+        @field:NotBlank
+        val title: String,
+
+        @field:NotBlank
+        val content: String,
+
+        @field:Pattern(regexp = "ALERT|EMERGENCY")
+        val type: String
+    )
+
+    data class Update(
+        @field:NotBlank
+        val password: String,
+
+        @field:NotBlank
+        val title: String,
+
+        @field:NotBlank
+        val content: String,
+
+        @field:Pattern(regexp = "ALERT|EMERGENCY")
+        val type: String
+    )
+
+    data class Delete(
+        @field:NotBlank
+        val password: String,
+    )
+
+}
