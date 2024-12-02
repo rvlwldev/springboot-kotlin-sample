1355e7a82c9ed5748a761f4906c2de1fdfbe7e51
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-12-02 09:57:55 +0900

    config: validation 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
 
