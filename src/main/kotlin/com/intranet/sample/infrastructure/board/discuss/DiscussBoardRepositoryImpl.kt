7c62eec635e4c2556163f3dce569d210384ec1ba
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-12-03 09:25:05 +0900

    feat: DiscussComment JPA, Repository 구현

--- src/main/kotlin/com/intranet/sample/infrastructure/board/discuss/DiscussBoardRepositoryImpl.kt
+++ src/main/kotlin/com/intranet/sample/infrastructure/board/discuss/DiscussBoardRepositoryImpl.kt
@@ -2,11 +2,30 @@ package com.intranet.sample.infrastructure.board.discuss
 
 import com.intranet.sample.domain.board.discuss.DiscussBoard
 import com.intranet.sample.domain.board.discuss.DiscussBoardRepository
+import com.intranet.sample.domain.board.discuss.DiscussComment
 import com.intranet.sample.infrastructure.board.AbstractBoardRepositoryImpl
 import com.intranet.sample.infrastructure.board.DefaultBoardJpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-class DiscussBoardRepositoryImpl(override val jpa: DefaultBoardJpaRepository<DiscussBoard>) :
-    AbstractBoardRepositoryImpl<DiscussBoard>(),
-    DiscussBoardRepository
+class DiscussBoardRepositoryImpl(
+    override val jpa: DefaultBoardJpaRepository<DiscussBoard>,
+    private val commentJpa: DiscussCommentJpaRepository,
+) : AbstractBoardRepositoryImpl<DiscussBoard>(), DiscussBoardRepository {
+
+    override fun findAllComments(boardId: Long): List<DiscussComment> =
+        commentJpa.findAllByBoardId(boardId)
+
+    override fun findAllComments(board: DiscussBoard): List<DiscussComment> =
+        commentJpa.findAllByBoardId(board.id)
+
+    override fun findComment(commentId: Long): DiscussComment? =
+        commentJpa.findById(commentId).orElse(null)
+
+    override fun saveComment(comment: DiscussComment): DiscussComment =
+        commentJpa.save(comment)
+
+    override fun deleteComment(comment: DiscussComment) =
+        commentJpa.delete(comment)
+
+}
---
+++ src/main/kotlin/com/intranet/sample/infrastructure/board/discuss/DiscussCommentJpaRepository.kt
@@ -0,0 +1,11 @@
+package com.intranet.sample.infrastructure.board.discuss
+
+import com.intranet.sample.domain.board.discuss.DiscussComment
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface DiscussCommentJpaRepository : JpaRepository<DiscussComment, Long> {
+
+    fun findAllByBoardId(boardId: Long): List<DiscussComment>
+    fun findByIdAndBoardId(boardId: Long, commentId: Long): DiscussComment?
+
+}
