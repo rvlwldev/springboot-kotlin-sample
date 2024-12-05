c3b2765fbbb2cabe5de6184de71b7cc5c1cef502
rvlwldev <hyungjun@gimhyeongjun-ui-noteubug.local>
2024-12-05 18:20:25 +0900

    config: GlobalControllerAdvice 에서 ResponseEntityExceptionHandler 상속 제외 (메서드 시그니처 중복)

--- src/main/kotlin/com/intranet/sample/configuration/exception/GlobalControllerAdvice.kt
+++ src/main/kotlin/com/intranet/sample/configuration/exception/GlobalControllerAdvice.kt
@@ -9,10 +9,9 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @ControllerAdvice
-class GlobalControllerAdvice : ResponseEntityExceptionHandler() {
+class GlobalControllerAdvice {
 
     private val logger: Logger = LoggerFactory.getLogger(GlobalControllerAdvice::class.java)
 
